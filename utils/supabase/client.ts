5ee5fdc19dd536b57164467fb27df74d4d7c537e
siegfriedbz <siegfried.bozza@yahoo.com>
2025-02-23 09:14:47 +0100

    cleanup

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+export const createClient = () => {
+  return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+};
